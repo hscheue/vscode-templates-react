fb70e9e2d3c1e67f00715fdd8847c97a7046be5c
hscheuerle <hscheue@gmail.com>
2024-04-01 08:01:32 -0400

    remove pages router templates

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,5 @@
 import { commands, type ExtensionContext } from "vscode";
 import newReactListReducerContext from "./commands/react/newReactListReducerContext";
-import newNextJsPage from "./commands/page-router/newNextJsPage";
-import newNextJsApi from "./commands/page-router/newNextJsApi";
 import newReactStateContext from "./commands/react/newReactStateContext";
 import extensionAppRouter from "./extensionAppRouter";
 import extensionReactComponent from "./extensionReactComponent";
@@ -21,19 +19,7 @@ export function activate(context: ExtensionContext) {
       newReactStateContext
     )
   );
-  // pages
-  context.subscriptions.push(
-    commands.registerCommand(
-      "vscode-templates-react.newNextJsPage",
-      newNextJsPage
-    )
-  );
-  context.subscriptions.push(
-    commands.registerCommand(
-      "vscode-templates-react.newNextJsApi",
-      newNextJsApi
-    )
-  );
+
   extensionAppRouter(context);
 }
 
